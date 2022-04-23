62bfb9534c411c13231cc2c314076cb1006fd198
zardfoever <87600155+ZardForever1009@users.noreply.github.com>
2022-04-23 21:33:25 +0800

    Update hw3-2.cpp

--- hw3-2.cpp
+++ hw3-2.cpp
@@ -61,9 +61,9 @@ void str_result(int symbol_count, Node* arr){
 	return;
 }
 
-// rebuild array to normal form (filled empty element with data '~')
+// rebuild array to normal form (move element with data '~' to last)
 void refactor_arr(int symbol_count, Node*& arr){
-	for(int i;i<symbol_count-1;i++){
+	for(int i=0;i<symbol_count-1;i++){
 		if(arr[i].data=='~'){
 			swap(arr[i], arr[i+1]);
 		}
@@ -71,13 +71,16 @@ void refactor_arr(int symbol_count, Node*& arr){
 	return;
 }
 
-// get minimum frequency node out of array
-Node* min_freq_node(int symbol_count, Node*& arr){
-	Node* min_node=new Node();
-	// find the minimum freq node
+// get minimum frequency node out of array (TRUE: end / FALSE: not end)
+bool min_freq_node(int symbol_count, Node*& arr, Node*& min_node){
+	int counter=0;
+	for(int i=0;i<symbol_count;i++){
+		if(arr[i].freq!=100000)counter++;
+	}
+	// if the node is only left one
 	int idx=0;
 	for(int i=0;i<symbol_count;i++){
-		if((arr[i].freq<min_node->freq)&&(arr[i].data!='~')){
+		if(arr[i].freq<min_node->freq&&arr[i].data!='~'){
 			*min_node=arr[i];
 			idx=i; // reset array item index
 		}
@@ -86,17 +89,93 @@ Node* min_freq_node(int symbol_count, Node*& arr){
 	arr[idx].freq=100000;
 	// refactor array
 	refactor_arr(symbol_count, arr);
-	return min_node;
+	if(counter==1)return true; // end
+	else return false;
+}
+
+// insert node back to array (for the parent node)
+void insert_node_arr(int symbol_count, Node*& arr, Node*& insert_node){
+	for(int i=0;i<symbol_count;i++){
+		if(arr[i].data=='~'&&arr[i].freq==100000){
+			arr[i]=*insert_node;
+			return;
+		}
+	}
+	cout<<"Insert error"<<endl; // for some unexpected error
+	return;
 }
 
 // build huffman tree
-void build_huffman_tree(int symbol_count, Node*& arr){
-	
+Node* build_huffman_tree(int symbol_count, Node*& arr){
+	bool huffman_done=false;
+	while(!huffman_done){
+		Node* left_n=new Node();  // smallest 
+		Node* right_n=new Node(); // second smallest
+		Node* insert_n=new Node();// the parent
+		// get the two smallest node
+		huffman_done=min_freq_node(symbol_count, arr, left_n);
+		if(huffman_done){
+			// release array memory
+			Node* result=left_n;
+			delete arr;
+			arr=nullptr;
+			return result;
+		}
+		min_freq_node(symbol_count, arr, right_n); // left_n must have something
+		// insert and update tree
+		insert_n->freq=left_n->freq+right_n->freq;
+		insert_n->left=left_n;
+		insert_n->right=right_n;
+		insert_node_arr(symbol_count, arr, insert_n);
+	}
+}
+
+// levelorder traversal
+bool level_order_traversal(Node* root, int level){
+	if (root==nullptr){
+        return false;
+    }
+    if (level==0){
+        return true;
+    }
+    bool left=level_order_traversal(root->left, level-1);
+    bool right=level_order_traversal(root->right, level-1);
+    return left||right; // still exist node not print
+}
+
+// preorder traversal (Root->Left->Right)
+void preorder_traversal(Node* root){
+	if(root==nullptr)return;
+	cout<<root->freq<<" ";
+	preorder_traversal(root->left);
+	preorder_traversal(root->right);
+}
+
+// inorder traversal (Left->Root->Right)
+void inorder_traversal(Node* root){
+	if(root==nullptr)return;
+	inorder_traversal(root->left);
+	cout<<root->freq<<" ";
+	inorder_traversal(root->right);
+}
+
+// tree level counter
+int level_counter(Node* root){
+	int level=0;
+	while(level_order_traversal(root, level)){
+		level++;
+	}
+	return level;
 }
 
 // print huffman tree result
-void huffman_result(){
+void huffman_result(Node* root, string code){
 	cout<<"Huffman Tree:\n";
+	cout<<"Preorder: ";
+	preorder_traversal(root);
+	cout<<"\nInorder: ";
+	inorder_traversal(root);
+	cout<<"\nLevel: "<<level_counter(root)<<endl;
 	
 	return;
 }
@@ -107,28 +186,16 @@ void huffman_coding(){
 	string str, code; // str: input string / code: huffman code
 	/* cin>>str; */
 	/* cin>>code; */
-	str="ccccccbbbaadddddddeeeeeeeeeevgsdfvdjsfhdsfhdsfhdsfh";
+	str="ccccccbbbaadddddddeeeeeeeeee";
 	code="110011000001";
 	int* alphabet=new int[26]; // store letters' appearance counts
 	int symbol_count=diff_char_count(str, alphabet); // different letters' counts
 	Node* arr=new Node[symbol_count]; // create Node array
 	build_node_arr(arr, alphabet);
+	delete[] alphabet; // release memory
 	str_result(symbol_count, arr);
-	
-	cout<<min_freq_node(symbol_count, arr)->data<<endl;
-	str_result(symbol_count, arr);
-	cout<<min_freq_node(symbol_count, arr)->data<<endl;
-	str_result(symbol_count, arr);
-	cout<<min_freq_node(symbol_count, arr)->data<<endl;
-	str_result(symbol_count, arr);
-	cout<<min_freq_node(symbol_count, arr)->data<<endl;
-	str_result(symbol_count, arr);
-	cout<<min_freq_node(symbol_count, arr)->data<<endl;
-	str_result(symbol_count, arr);
-	cout<<min_freq_node(symbol_count, arr)->data<<endl;
-	str_result(symbol_count, arr);
-	
-	huffman_result();
+	Node* root=build_huffman_tree(symbol_count, arr);
+	huffman_result(root, code);
 	return;
 }
 
